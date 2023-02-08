8d0c251b7731d0cf4e19948a16af31b7c3129560
Fl1ck <77853166+FlickNoJutsu@users.noreply.github.com>
2023-02-07 21:48:45 -0500

    Update Wrong URL.ml

--- fl1ck.ml/Wrong URL.ml
+++ fl1ck.ml/Wrong URL.ml
@@ -5,3 +5,21 @@ Please contact the sender and tell them what's happening.
 Fl1ck.ml
 
 Made by Fl1ck
+
+--
+
+Checkout my other projects :
+
+Fl1ck.ml Welcome Page
+https://github.com/FlickNoJutsu/FlickNoJutsu/blob/main/fl1ck.ml/fl1ck.ml
+
+Best FREE Roblox Injector [Working as of 28/01/2023]
+https://github.com/FlickNoJutsu/Rexploit
+
+All In One Cheat Loader (Roblox, Minecraft & More...) [DISCONTINUED]
+https://github.com/FlickNoJutsu/CatBroHubv1
+
+---
+
+Buy me a coffee :)
+https://www.buymeacoffee.com/fl1ckcs
