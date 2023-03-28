5a9f62f8b98d0bbf506f0e5a49559cea49006ef7
Fl1ck <77853166+FlickNoJutsu@users.noreply.github.com>
2023-03-28 18:13:22 -0400

    Update valorant.ml

--- fl1ck.ml/valorant.ml
+++ fl1ck.ml/valorant.ml
@@ -1,7 +1,7 @@
-https://fl1ck.ml/valorant
+https://fl1ck.tk/valorant
 
 All my valorant accounts :
-Main - peaked dia 3 | idrinkperiodblud#blood
+Main - Diamond 3 | idrinkperiodblud#blood
 Smurf #1 - plat 1 | idrinkperiodblud#fl1ck
 Smurf #2 - silver 3 | Fl1ck#alt
-
+Smurf #3 - Silver 1 | I peaked iron 3#real
