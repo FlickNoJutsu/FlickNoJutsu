730b8d7a7a92c257cc6c0dbdcadb7ccd4eb746ff
Fl1ck <77853166+FlickNoJutsu@users.noreply.github.com>
2023-04-07 21:52:13 -0400

    Update valorant.ml
    New name new rank

--- fl1ck.ml/valorant.ml
+++ fl1ck.ml/valorant.ml
@@ -1,7 +1,7 @@
 https://fl1ck.tk/valorant
 
 All my valorant accounts :
-Main - Diamond 3 | idrinkperiodblud#blood
+Main - Ascendant 1 | His F1eshlight#fl1ck
 Smurf #1 - plat 1 | idrinkperiodblud#fl1ck
 Smurf #2 - silver 3 | Fl1ck#alt
 Smurf #3 - Silver 1 | I peaked iron 3#real
